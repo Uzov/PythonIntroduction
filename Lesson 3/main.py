7471f79d0b691219b34f4458e0c38f8d39afb8ed
Evgeny Uzov <uzov@cg.ru>
2023-08-25 23:46:31 +0300

    Lesson 3 done

--- Lesson 3/main.py
+++ Lesson 3/main.py
@@ -45,25 +45,43 @@ def count(list_1: list, num: int) -> int:
     return len(list(filter(lambda x: x == num, list_1)))
 
 
+"""точный способ!"""
+
+
 def closer(list_1: list, num: int) -> list:
     lst = list(map(lambda x: abs(num - x), list_1))
     mi = min(lst)
     return [i for i in range(0, len(lst)) if lst[i] == mi]
 
 
-# как оказывается, способ не совсем точный!
+"""не точный способ!"""
+
+
 def closer1(list_1: list, num: int) -> list:
     mi = min(list_1, key=lambda x: abs(num - x))
     return [i for i in range(0, len(list_1)) if list_1[i] == mi]
 
 
-points: dict[int, list[str]] = {1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
-                                2: ['D', 'G'],
-                                3: ['B', 'C', 'M', 'P'],
-                                4: ['F', 'H', 'V', 'W', 'Y'],
-                                5: ['K'],
-                                8: ['J', 'X'],
-                                10: ['Q', 'Z']}
+def getpoint(value) -> int:
+    points: dict[int, list[str]] = {1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
+                                    2: ['D', 'G'],
+                                    3: ['B', 'C', 'M', 'P'],
+                                    4: ['F', 'H', 'V', 'W', 'Y'],
+                                    5: ['K'],
+                                    8: ['J', 'X'],
+                                    10: ['Q', 'Z']}
+    for point, lst in points.items():
+        for i in lst:
+            if i == value:
+                return point
+
+
+def scrabble(value: list) -> int:
+    summa = 0
+    for itm in value:
+        summa = summa + getpoint(itm)
+    return summa
+
 
 if __name__ == '__main__':
     print(count([1, 2, 3, 4, 5, 3, 4, 5, 6, 7, 3, 2, 2, 4, 5, 7, 6, 4, 1, 3, 3, 34, 5, 6, 7, 0, 4, 0, 3, 3, 5, 6], 3))
@@ -71,3 +89,6 @@ if __name__ == '__main__':
     print(closer1([1, 2, 3, 4, 5, 8, 3, 5, 6, 8, 4, 2, 10], 7))
     print(closer([1, 2, 3, 4, 5, 7, 8, 3, 5, 6, 8, 4, 2, 10], 7))
     print(closer1([1, 2, 3, 4, 5, 7, 8, 3, 5, 6, 8, 4, 2, 10], 7))
+    #4+1+1+4+1+1+2
+    print(scrabble('FORWARD'))
+    print(scrabble("PYTHONIST"))
