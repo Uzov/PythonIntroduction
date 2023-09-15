98afde081e0014ff846e12a16ae963e372c6bc76
Evgeny Uzov <uzov@cg.ru>
2023-09-15 23:22:54 +0300

    Lesson 8 has been done

--- Lesson 8/main.py
+++ Lesson 8/main.py
@@ -14,10 +14,12 @@
 import pandas as pd
 import numpy as np
 
-def display(in_func): # Декоратор для печати DataFrame
+
+def display(in_func):  # Заготовка декоратора для печати DataFrame, пригодится
     def out_function(*args):
         print(dict(enumerate(in_func(*args))))
     return out_function
+
 
 class Person:
     __type = "Person"
@@ -59,7 +61,7 @@ class Phonebook:
         self.__filename = filename
 
     @property
-    def file(self) -> str: # Узнать имя файла текущей телефонной книги
+    def file(self) -> str:  # Узнать имя файла текущей телефонной книги
         return self.__filename
 
     def add(self, person: Person, phone_number: str) -> None:  # Добавляет запись в конец файла
@@ -75,57 +77,72 @@ class Phonebook:
             print(f'Запись {person} с телефонным номером: {phone_number} не добавлена!')
             print(f'Произошла следующая ошибка: {error}!')
 
-    def read(self): # Читает и выводит весь файл
+    def read(self):  # Читает и выводит весь файл
         df = pd.read_csv(self.__filename, delimiter=';',
-                         names=["Фамилия:", "Имя:", "Отчество:", "Описание:", "Телефон:"]).fillna("")
+                         names=["Фамилия:", "Имя:", "Отчество:",
+                                "Описание:", "Телефон:"]).fillna("")  # Попутно заполняем NaN пустыми строками
         print(f'Найдено {len(df)} записей:\n{df}')
 
-    def find(self, display=True, *args):  # Ищет записи, если число критериев больше нуля
+    def find(self, disp=True, *args):  # Ищет записи, если число критериев больше нуля
         if len(args) == 0:
             print(f'Записи не могут быть найдены! Введите хотя бы один аргумент!')
             return
         df = pd.read_csv(self.__filename, delimiter=';',
-                         names=["Фамилия:", "Имя:", "Отчество:", "Описание:", "Телефон:"]).fillna("")
+                         names=["Фамилия:", "Имя:", "Отчество:", "Описание:", "Телефон:"])
         lst = [*args]
         for col in lst:
             df = df[df.isin([col, ]).any(axis=1)]
-        if display:
+        if disp:
             print(f'Найдено {len(df)} записей:\n{df}')
         else:
             return df
 
-    def update(self, *args):  # Обновляет запись
+    def update(self, phone_number, *args):  # Обновляет запись
         df = self.find(False, *args)
         if len(df) == 0:
             print(f'Запись для обновления не найдена, нечего обновлять!')
+            return
         elif len(df) > 1:
             print(f'Найдено {len(df)} записей:\n{df}')
-            input("Выберите id записи для обновления: ")
+            item = int(input("Выберите id записи для обновления: ")) - 1
+            df = pd.read_csv(self.__filename, delimiter=';')
+            try:
+                df.at[item, 'phone_number'] = phone_number
+                columns = ['second_name', 'first_name', 'surname', 'description', 'phone_number']
+                df.to_csv(self.__filename, mode='w', index=False, header=True, sep=";", columns=columns)
+                print(f'Запись "{df.loc[[item]]}" успешно изменена!')
+            except Exception as error:
+                print(f'Произошла ошибка обновления записи: {error}!')
 
-    def delete(self, *args):  # Стирает запись
-        df = self.find(False, *args)
+    def delete(self, *args) -> None:  # Стирает запись
+        df = self.find(False, *args)  # Ищем а есть ли вообще записи для удаления?
         if len(df) == 0:
             print(f'Запись для удаления не найдена, нечего удалять!')
+            return
         elif len(df) > 1:
             print(f'Найдено {len(df)} записей:\n{df}')
-            lst = [int(x) for x in input("Введите через пробел id записей для удаления: ").split()]
-        df = pd.read_csv(self.__filename, delimiter=';')
-        df = df.drop(labels=lst, axis=0)
-        print(df)
+            lst = [int(x) - 1 for x in input("Введите через пробел id записей для удаления: ").split()]
+            df = pd.read_csv(self.__filename, delimiter=';')  # Читаем весь файл
+            try:
+                df = df.drop(labels=lst, axis=0)
+                columns = ['second_name', 'first_name', 'surname', 'description', 'phone_number']
+                df.to_csv(self.__filename, mode='w', index=False, header=True, sep=";", columns=columns)
+                print(f'Записи {lst} успешно удалены!')
+            except Exception as error:
+                print(f'Произошла ошибка удаления записи: {error}!')
 
 
-
-if __name__ == "__main__": # Проверка
+if __name__ == "__main__":  # Проверка
     p1 = Person(first_name="Вася", second_name="Пупкин", desc="Тестовый пользователь")
-    #print(p1)
-    #p2 = Person(first_name="Женя")
-    #p2.second_name = "Юзов"
-    #print(p2)
+    # print(p1)
+    # p2 = Person(first_name="Женя")
+    # p2.second_name = "Юзов"
+    # print(p2)
     ph = Phonebook("textfile.csv")
-    #ph.add(p1, '+7 916 329-55-16')
+    # ph.add(p1, '+7 916 329-55-16')
     ph.find(True, "Вася")
     ph.find(True, "+7 499 586-05-91")
-    #ph.read()
-    #ph.update("Людмила", "Игоревна")
-    #ph.delete("Людмила", "Игоревна")
-    ph.delete("Вася")
+    # ph.read()
+    # ph.update("Людмила", "Игоревна")
+    ph.update("+7 843 222-22-22", "Людмила")
+    # ph.delete("Вася")
